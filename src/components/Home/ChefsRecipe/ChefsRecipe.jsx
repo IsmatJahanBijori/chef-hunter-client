535b0ba007d87c3eaf97188bc377aa5d90e5055d
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-05-03 23:49:33 +0600

    chefsInfo banner added

--- src/components/Home/ChefsRecipe/ChefsRecipe.jsx
+++ src/components/Home/ChefsRecipe/ChefsRecipe.jsx
@@ -1,12 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Card, Container } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const ChefsRecipe = () => {
-    const chefsInfo=useLoaderData()
+
+    const chefsInfo = useLoaderData()
     // console.log(chefsInfo)
     return (
-        <h2>{chefsInfo.name}</h2>
+        <Container>
+            <Card className='mt-5 p-5'>
+                <Card.Img style={{ width: "1200px", height: '800px'}} variant="top" src={chefsInfo.picture} />
+                <Card.Body style={{ fontFamily: 'Manrope', fontSize: '30px' }}>
+                    <Card.Text>{chefsInfo.name}</Card.Text>
+                    <Card.Text>Experience: {chefsInfo.experience}</Card.Text>
+                    <Card.Text>Numbers of Recipes:: {chefsInfo.numberOfRecipes}</Card.Text>
+                    <Card.Text>Likes: {chefsInfo.likes}</Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 };
 
